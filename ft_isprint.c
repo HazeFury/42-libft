62fbf6e9960707619296acb649810049f188083c
HazeFury <mab.dev69@gmail.com>
2025-11-10 17:01:33 +0100

    main

---
+++ ft_isprint.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+// #include <ctype.h>
+
+int	ft_isprint(int c)
+{
+	if ((c >= 32 && c <= 126))
+		return (1);
+	return (0);
+}
+
+/*
+int	main(void)
+{
+	printf("%d\n", ft_isprint(25)); // = 0
+	printf("%d\n", ft_isprint(98)); // = 1
+	
+	printf("%d\n", isprint(25)); // = 0
+	printf("%d\n", isprint(98)); // = 1
+}
+*/
