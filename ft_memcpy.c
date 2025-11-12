c84c86725f1497764934973c99e74af1ac2acd38
Marc-Antoine Berger <marberge@z2r11p2.42lyon.fr>
2025-11-12 19:35:19 +0100

    WIP on ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,28 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marberge <marberge@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/12 16:11:18 by marberge          #+#    #+#             */
-/*   Updated: 2025/11/12 17:19:58 by marberge         ###   ########lyon.fr   */
+/*   Created: 2025/11/12 17:21:04 by marberge          #+#    #+#             */
+/*   Updated: 2025/11/12 19:34:35 by marberge         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stdio.h>
+#include <string.h>
 
-# include <stddef.h>
+/*
+void	*memcpy(void *dest, const void *src, size_t n)
+{
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-void	bzero(void *s, size_t n);
-void	*memmove(void *dest, const void *src, size_t n);
+}
+*/
 
-#endif
+int	main(void)
+{
+	char	src[] = "ABCDE";
+	printf("Before : %s\n", src);
+	memcpy(src + 1, src,  4);
+	printf("After  : %s\n", src);
+	printf("\n");
+}
