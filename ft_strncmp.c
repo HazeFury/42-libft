62b2d73cda991712a2241e1b8f82ebe533d8999e
Marc-Antoine Berger <marberge@z2r8p4.42lyon.fr>
2025-11-14 16:34:43 +0100

    ft_memchr

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: marberge <marberge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 15:23:30 by marberge          #+#    #+#             */
-/*   Updated: 2025/11/14 15:50:50 by marberge         ###   ########.fr       */
+/*   Updated: 2025/11/14 16:20:52 by marberge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-	unsigned char *	us1;
-	unsigned char *	us2;
+	size_t			i;
+	unsigned char	*us1;
+	unsigned char	*us2;
 
 	us1 = (unsigned char *)s1;
 	us2 = (unsigned char *)s2;
@@ -32,11 +32,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 /*
 int	main(void)
 {
-	char		str1[] = "ABC";
-	char		str2[] = "ABC";
+	char		s1[] = "ABC";
+	char		s2[] = "ABC";
 	unsigned int	n = 4;	
 	
-	printf("<%s> - <%s> (%d) =  %d\n", str1, str2, n, ft_strncmp("test\200", "test\0", 6));
+	printf("<%s> - <%s> (%d) =  %d\n", s1, s2, n, ft_strncmp(s1, s2, n));
 	return (0);
 }
 */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: marberge <marberge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 16:11:18 by marberge          #+#    #+#             */
-/*   Updated: 2025/11/14 15:39:53 by marberge         ###   ########.fr       */
+/*   Updated: 2025/11/14 16:03:25 by marberge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
