406050f0d64cb953082f599abdd30acad3cdb3b1
Marc-Antoine Berger <marberge@z1r10p2.42lyon.fr>
2025-11-21 19:21:43 +0100

    ft_lstlast

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: marberge <marberge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 17:27:29 by marberge          #+#    #+#             */
-/*   Updated: 2025/11/21 18:20:54 by marberge         ###   ########.fr       */
+/*   Updated: 2025/11/21 19:20:45 by marberge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 
 // int	main(void)
 // {
-// 	t_list	**list;
+// 	t_list	*list;
 // 	t_list	*node;
 // 	t_list	*node2;
 // 	char	*s = "world";
@@ -30,11 +30,17 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 // 	list = NULL;
 // 	node = ft_lstnew(s);
 // 	node2 = ft_lstnew(s2);
-// 	list = &node;
-// 	printf("n1 : %p | %s |  %p\n", &node, (char *)node->content, node->next);
-// 	printf("n2 : %p | %s |  %p\n", &node2, (char *)node2->content, node2->next);
-// 	printf("list adress (first node) : %p\n", *list);
-// 	ft_lstadd_front(list, node2);
-// 	printf("list adress (first node) : %p\n", *list);
+// 	printf("n1 : %p | %s |  %p\n", node, (char *)node->content, node->next);
+// 	printf("n2 : %p | %s |  %p\n", node2, (char *)node2->content, node2->next);
+// 	printf("list adress (first node) : %p\n", list);
+// 	ft_lstadd_front(&list, node);
+// 	ft_lstadd_front(&list, node2);
+// 	printf("\n");
+// 	printf("list adress (first node) : %p\n", list);
+// 	while (list)
+// 	{
+// 		printf("%p | %s |  %p\n", list, (char *)list->content, list->next);
+// 		list = list->next;
+// 	}
 // 	return (0);
 // }
