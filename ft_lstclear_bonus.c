6f49267b896717c92a3919ebf039c198a9317eac
Marc-Antoine Berger <marberge@z1r10p2.42lyon.fr>
2025-11-22 14:44:55 +0100

    ft_lstclear

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,12 +6,12 @@
 /*   By: marberge <marberge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 12:23:44 by marberge          #+#    #+#             */
-/*   Updated: 2025/11/22 13:43:36 by marberge         ###   ########.fr       */
+/*   Updated: 2025/11/22 14:44:43 by marberge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+// #include <stdio.h>
 
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
@@ -39,48 +39,48 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	*lst = NULL;
 }
 
-static void	ft_delcontent(void *content)
-{
-	if (content)
-		free(content);
-}
+// static void	ft_delcontent(void *content)
+// {
+// 	if (content)
+// 		free(content);
+// }
 
-static void	ft_print_list(t_list *lst)
-{
-	t_list	*tmp;
+// static void	ft_print_list(t_list *lst)
+// {
+// 	t_list	*tmp;
 
-	tmp = lst;
-	if (!tmp)
-		printf("ce noeud est NULL");
-	while (tmp)
-	{
-		printf("%p | %s |  %p\n", tmp, (char *)tmp->content, tmp->next);
-		tmp = tmp->next;
-	}
-	printf("\n");
-}
+// 	tmp = lst;
+// 	if (!tmp)
+// 		printf("ce noeud est NULL");
+// 	while (tmp)
+// 	{
+// 		printf("%p | %s |  %p\n", tmp, (char *)tmp->content, tmp->next);
+// 		tmp = tmp->next;
+// 	}
+// 	printf("\n");
+// }
 
-int	main(void)
-{
-	t_list	*list;
-	t_list	*node;
-	t_list	*node2;
-	t_list	*node3;
-	t_list	*tmp;
+// int	main(void)
+// {
+// 	t_list	*list;
+// 	t_list	*node;
+// 	t_list	*node2;
+// 	t_list	*node3;
 
-	list = NULL;
-	node = ft_lstnew(ft_strdup("hello"));
-	node2 = ft_lstnew(ft_strdup("world"));
-	node3 = ft_lstnew(ft_strdup("Marco"));
-	ft_lstadd_front(&list, node);
-	ft_lstadd_back(&list, node2);
-	ft_lstadd_back(&list, node3);
-	ft_print_list(list);
-	tmp = list;
-	// tmp = tmp->next;
-	ft_lstclear(&tmp, ft_delcontent);
-	// tmp = list;
-	// tmp->next = NULL;
-	ft_print_list(list);
-	return (0);
-}
+// 	list = NULL;
+// 	node = ft_lstnew(ft_strdup("hello"));
+// 	node2 = ft_lstnew(ft_strdup("world"));
+// 	node3 = ft_lstnew(ft_strdup("Marco"));
+// 	ft_lstadd_front(&list, node);
+// 	ft_lstadd_back(&list, node2);
+// 	ft_lstadd_back(&list, node3);
+// 	printf("BEFORE : %p\n", list);
+// 	ft_print_list(list);
+// 	ft_lstclear(&list, ft_delcontent);
+// 	if (!list)
+// 	{
+// 		printf("AFTER : %p\n", list);
+// 		printf("ok !\n");
+// 	}
+// 	return (0);
+// }
